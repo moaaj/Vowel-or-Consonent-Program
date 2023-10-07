b274fd9bd603864433d6c74468e0ff8452b55d2d
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2023-10-08 00:39:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Vowel // Consonant
+
+var letter = prompt("Enter a letter : ");
+
+letter = letter.toLowerCase();
+
+if(letter=="a" || letter =="e" || letter =="i" ||  letter=="o" || letter=="u")
+console.log("Vowel");
+
+
+else
+console.log("Consonent");
